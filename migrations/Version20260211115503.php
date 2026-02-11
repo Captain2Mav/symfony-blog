89f77a23684b21a922c2e488b9829300b7444418
lenny <lennyzepho92@gmail.com>
2026-02-11 14:54:31 +0100

    l'administrateur peut maintenant publier chaquearticle sur la liste

--- migrations/Version20260211115503.php
+++ migrations/Version20260211115503.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260202114620 extends AbstractMigration
+final class Version20260211115503 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20260202114620 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_64C19C15E237E06 (name), UNIQUE INDEX UNIQ_64C19C1989D9B62 (slug), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE category DROP description');
+        $this->addSql('ALTER TABLE post ADD description VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE category');
+        $this->addSql('ALTER TABLE category ADD description TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE post DROP description');
     }
 }
